f3f6e26bcb36188f340737d1df783b6cb4a864df
Vikrant Malla <vikrantmalla999@gmail.com>
2021-12-13 19:54:24 +0545

    add email-validation fun

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = form['email'].value.trim();
+
+
+    if (email === '') {
+        setErrorMsg('email', 'Email cannot be empty');
+    } else if (!isValid(email)) {
+        setErrorMsg('email', 'Check your email please');
+    } else {
+        removeErrorMsg('email');
+    }
+});
+
+function setErrorMsg(field, message) {
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function removeErrorMsg(field) {
+    const formControl = form[field].parentNode;
+    formControl.classList.remove('error');
+    formControl.classList.add('success');
+}
+
+function isValid(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
